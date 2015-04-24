1b33ed9d0686dc0b8f3e35068575186773d91041
Filip Bruun Bech-Larsen <fbruun@skybrud.dk>
2015-04-24 12:44:59 +0200

    Initial commit

---
+++ sky-sharethis.directive.ts
@@ -0,0 +1,44 @@
+/* global angular, window*/
+(function() {
+	'use strict';
+	/**
+	 * This is the skySharethis directive
+	 * Usage <button sky-sharethis="facebook" apikey="rtqsa3gsn5td9u6uvb43cvyh">share on facebook</button>
+	 *
+	 * The value of the attribute "sky-sharethis" is the sharing destination
+	 * List of available destinations here: http://sharethis.com/publishers/services-directory
+	 *
+	 * The (optional) value of the attribute "apikey" is the users apikey for sharethis
+	 * Sign up for apikey here: http://developer.sharethis.com/apps/register
+	 *
+	 * Tools:
+	 * http://developer.sharethis.com/io-docs
+	 */
+
+	angular.module('skySharethis').directive('skySharethis',skySharethis);
+
+	skySharethis.$inject = ['sharethisapi','$window'];
+
+	function skySharethis(sharethisapi, $window) {
+		var directive = {
+			restrict:'A',
+			link:link
+		};
+
+		function link(scope,element,attrs) {
+			element[0].title=attrs.skySharethis;
+			element.on('click', function() {
+				var apikey = attrs.apikey || sharethisapi;
+				$window.open(
+					'http://rest.sharethis.com/v1/share/share?destination='+encodeURIComponent(attrs.skySharethis)+
+						'&url='+encodeURIComponent($window.location.href)+
+						'&api_key='+encodeURIComponent(apikey),
+					'Share this',
+					'status=0,toolbar=0,width=500,height=400'
+				);
+			});
+		}
+
+		return directive;
+	}
+})();
---
+++ sky-sharethis.module.ts
@@ -0,0 +1,8 @@
+/* global angular, window*/
+(function() {
+	'use strict';
+
+	angular.module('skySharethis',[]);
+	angular.module('skySharethis').constant('sharethisapi','rtqsa3gsn5td9u6uvb43cvyh');
+
+})();
